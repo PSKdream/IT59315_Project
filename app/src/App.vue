a5fe68475000533f61c2d3cd72dfa02a5b85f007
View <thanathip804@gmail.com>
2022-04-09 23:44:41 +0700

    pull

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,23 +1,21 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar>
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+    </v-toolbar>
+    <v-footer fixed>
+      <span>Vuetify $copy; 2017</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
-</script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
