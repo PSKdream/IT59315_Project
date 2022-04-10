330faf5ef2543da36d9f248a53e51c0c456eece3
View <thanathip804@gmail.com>
2022-04-10 18:22:19 +0700

    navbar

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,69 +1,21 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <!-- <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        /> -->
-      </div>
-
+    <v-app-bar class="d-flax align-center" app color="primary">
+      <v-app-bar-nav-icon color="black"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-title >Cryptography Project</v-toolbar-title>
       <v-spacer></v-spacer>
     </v-app-bar>
 
-     <form>
-    <v-container fluid>
-    <v-text-field
-      label="Input a plain text"
-      required
-    ></v-text-field>
-
-    <v-text-field
-      name="text_EN"
-      label="Input a plain text"
-      required
-    ></v-text-field> 
-
-      <v-text-field
-      name="kvalue"
-      label="Input a K value"
-      required
-    ></v-text-field> 
-
-    <v-textarea
-      name="Result"
-      filled
-      label="Result"
-      auto-grow
-    ></v-textarea>
-
-    <v-btn
-      class="mr-4"
-      @click="submit"
-    >
-      submit
-    </v-btn>
-    <v-btn @click="clear">
-      clear
-    </v-btn>
-    </v-container>
-  </form>
+    <v-main>
+      <router-view />
+    </v-main>
   </v-app>
-  
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
   data: () => ({
     //
