6325dfa39069be7dcb4df54e2b000839ada69ba6
View <thanathip804@gmail.com>
2022-04-10 12:45:51 +0700

    form

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,63 @@
 <template>
   <v-app>
-    
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <!-- <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        /> -->
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+     <form>
+    <v-container fluid>
+    <v-text-field
+      label="Input a plain text"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      name="text_EN"
+      label="Input a plain text"
+      required
+    ></v-text-field> 
+
+      <v-text-field
+      name="kvalue"
+      label="Input a K value"
+      required
+    ></v-text-field> 
+
+    <v-textarea
+      name="Result"
+      filled
+      label="Result"
+      auto-grow
+    ></v-textarea>
+
+    <v-btn
+      class="mr-4"
+      @click="submit"
+    >
+      submit
+    </v-btn>
+    <v-btn @click="clear">
+      clear
+    </v-btn>
+    </v-container>
+  </form>
   </v-app>
+  
 </template>
 
 <script>
--- server/routes/ColumnarTransposition.py
+++ server/routes/ColumnarTransposition.py
@@ -11,18 +11,10 @@ route = APIRouter()
 @route.post('/ColumnarTransposition/encrypt')
 async def encrypt(cts: schema):
     obj = algorithm.ColumnarTransposition()
-<<<<<<< HEAD
-    return obj.encrypt(cts.text,cts.key)
-=======
     return obj.encrypt(cts.text, cts.key)
 
->>>>>>> ea7699b264d38236f55254d352c37230392727b6
 
 @route.post('/ColumnarTransposition/decrypt')
 async def encrypt(cts: schema):
     obj = algorithm.ColumnarTransposition()
-<<<<<<< HEAD
-    return obj.decrypt(cts.text,cts.key)
-=======
     return obj.decrypt(cts.text, cts.key)
->>>>>>> ea7699b264d38236f55254d352c37230392727b6
