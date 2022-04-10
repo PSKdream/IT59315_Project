c726599810cffc15dd8e23f30b5179021ce4cd54
View <thanathip804@gmail.com>
2022-04-10 10:27:11 +0700

    set app

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <v-app>
-    <v-toolbar>
-      <v-toolbar-title>Vuetify</v-toolbar-title>
-    </v-toolbar>
-    <v-footer fixed>
-      <span>Vuetify $copy; 2017</span>
-    </v-footer>
+    
   </v-app>
 </template>
 
--- server/routes/ColumnarTransposition.py
+++ server/routes/ColumnarTransposition.py
@@ -9,9 +9,9 @@ ct = APIRouter()
 @ct.post('/ColumnarTransposition/encrypt')
 async def encrypt(cts:schema):
     obj = algorithm.ColumnarTransposition()
-    return obj.encrypt(cts.plan_text,cts.key)
+    return obj.encrypt(cts.text,cts.key)
 
 @ct.post('/ColumnarTransposition/decrypt')
 async def encrypt(cts:schema):
     obj = algorithm.ColumnarTransposition()
-    return obj.decrypt(cts.plan_text,cts.key)
+    return obj.decrypt(cts.text,cts.key)
