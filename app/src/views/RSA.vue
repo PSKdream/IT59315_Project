447c6baeaa5f9dbd8535cae4347d630da443fd70
Phatthadon Maliwan <pattadon.mali.43@gmail.com>
2022-04-11 21:13:41 +0700

    add form

--- app/src/views/RSA.vue
+++ app/src/views/RSA.vue
@@ -1,6 +1,45 @@
 <template>
-  <div class="RSA">
-    <h1>RSA</h1>
+  <div class="RSA mt-4">
+    <h1 class="text-center">RSA</h1>
+    <v-card elevation="4" max-width="600" class="mx-auto my-4 pa-3">
+      <v-textarea
+        filled
+        auto-grow
+        label="Input plain text"
+        class="mx-4 mt-4"
+      ></v-textarea>
+      <v-textarea
+        filled
+        auto-grow
+        label="Input key value"
+        class="mx-4"
+      ></v-textarea>
+      <v-textarea
+        filled
+        auto-grow
+        label="Result"
+        class="mx-4"
+        disabled
+      ></v-textarea>
+      <v-row class="ml-4 my-auto mb-4">
+        <v-btn 
+          color="#0061BB" 
+          class="mr-4 white--text"
+          width="265"
+          height="50"
+        >
+        Encrypt
+        </v-btn>
+        <v-btn
+          color="#FF4D4D"
+          class="mr-4 white--text"
+          width="265"
+          height="50"
+        >
+        Decrypt
+        </v-btn>
+      </v-row>
+    </v-card>
   </div>
 </template>
 
--- app/src/views/VigenereCipher.vue
+++ app/src/views/VigenereCipher.vue
@@ -1,6 +1,63 @@
 <template>
-  <div class="VigenereCipher">
-    <h1>Vigenere Cipher</h1>
+  <div class="VigenereCipher mt-4">
+    <h1 class="text-center">Vigenere Cipher</h1>
+    <v-card elevation="4" max-width="600" class="mx-auto my-4 pa-3">
+      <v-textarea
+        filled
+        auto-grow
+        label="Input plain text"
+        class="mx-4 mt-4"
+      ></v-textarea>
+      <v-textarea
+        filled
+        auto-grow
+        label="Input key value"
+        class="mx-4"
+      ></v-textarea>
+      <v-textarea
+        filled
+        auto-grow
+        label="Result"
+        class="mx-4"
+        disabled
+      ></v-textarea>
+      <!-- <v-text-field
+        v-model="name"
+        :rules="nameRules"
+        label="Input plain text"
+        class="mx-4"
+        required
+      ></v-text-field>
+      <v-text-field
+        label="Input key value"
+        class="mx-4"
+        required
+      ></v-text-field>
+      <v-text-field
+        label="Result"
+        class="mx-4"
+        filled
+        disabled
+      ></v-text-field> -->
+      <v-row class="ml-4 my-auto mb-4">
+        <v-btn 
+          color="#0061BB" 
+          class="mr-4 white--text"
+          width="265"
+          height="50"
+        >
+        Encrypt
+        </v-btn>
+        <v-btn
+          color="#FF4D4D"
+          class="mr-4 white--text"
+          width="265"
+          height="50"
+        >
+        Decrypt
+        </v-btn>
+      </v-row>
+    </v-card>
   </div>
 </template>
 
