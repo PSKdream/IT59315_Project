9001bdcff2ced5efad770f83294262f540d3eef9
PSK-Dream <49468390+PSKdream@users.noreply.github.com>
2022-04-27 23:07:13 +0700

    fix bug ColumnarTransposition encrypt

--- server/algorithm/columnar_transposition.py
+++ server/algorithm/columnar_transposition.py
@@ -20,22 +20,37 @@ class ColumnarTransposition:
             p.append('')
 
         p = np.reshape(p, (-1, n))
+
         p = p[:, key]
+        print(p)
         c = p.T.flatten()
 
         return ''.join(c)
 
     def decrypt(self, cipher_text: str, key: str = '1,2,3'):
-        key = self.get_key(key) - 1  # num of col
+        key = self.get_key(key)  # num of col
         c = list(cipher_text)
         n = len(key)
         m = math.ceil(len(c) / n)
-
         c_n = len(c)
-        for ind, index in enumerate(reversed(key)):
-            if index > (c_n % n) - 1:
-                c.insert(((n - ind) * m) - 1, '')
+        arr = np.full((m, len(key)), '')
+        start, end = 0, 0
+        for ind, text in enumerate(key):
+            if text-1 >= c_n % (m - 1):
+                end = m-1
+                temp = c[start:end]
+                c = c[end:]
+            else:
+                end = m
+                temp = c[start:end]
+                c = c[end:]
+            arr[:len(temp), ind] = temp
 
-        c = np.reshape(c, (-1, n), order='F')
-        p = c[:, np.argsort(key)].flatten()
+        p = arr[:, np.argsort(key)].flatten()
         return ''.join(p)
+
+
+# obj = ColumnarTransposition()
+# key = '2,1,3'
+# en = obj.encrypt('PanyapiwatInstitute', key)
+# print(obj.decrypt(en, key))
